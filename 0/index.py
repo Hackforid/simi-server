84d4a3f1ad2fbefa6c0d5f34e4200c42a98dc325
yuanbowen <yuanbowen@douban.com>
2013-08-27 23:22:54 +0800

    find tornado version

--- 0/index.py
+++ 0/index.py
@@ -1,12 +1,13 @@
 #-*- coding:utf-8 -*-
 
 import tornado.wsgi
+import tornado
 
 
 class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write("Hello, world! - Tornado\n")
+        self.write(tornado.version)
 
 app = tornado.wsgi.WSGIApplication([(r"/", MainHandler), ])
 from bae.core.wsgi import WSGIApplication
