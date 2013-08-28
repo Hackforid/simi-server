356d97acfe8162335d9e8b63274ef24076257eaa
yuanbowen <yuanbowen@douban.com>
2013-08-28 13:52:00 +0800

    fuck zhouquan

--- 0/index.py
+++ 0/index.py
@@ -1,13 +1,12 @@
 #-*- coding:utf-8 -*-
 
 import tornado.wsgi
-import tornado
 
 
 class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write(tornado.version)
+        self.write({'warning':'fuck zhouquan!!!'})
 
 app = tornado.wsgi.WSGIApplication([(r"/", MainHandler), ])
 from bae.core.wsgi import WSGIApplication
